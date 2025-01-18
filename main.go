c1ab9dc2fd725c18e4f18a507e9e2bb343e88bd6
Kashish Sahu <125540624+gitKashish@users.noreply.github.com>
2025-01-19 04:22:57 +0530

    Added ability to write to target file and flags for source, target and showing formatted logs

--- main.go
+++ main.go
@@ -3,16 +3,17 @@ package main
 import (
 	"bufio"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"os"
 	"regexp"
 	"strings"
 )
 
-// LogEntry represents a structured log entry
+// Represents a structured log entry
 type LogEntry struct {
 	Timestamp string                 `json:"timestamp,omitempty"`
-	PmId      string                 `json:"pmid,omitempty`
+	PmId      string                 `json:"pmid,omitempty"`
 	Server    string                 `json:"server,omitempty"`
 	Module    string                 `json:"module,omitempty"`
 	Api       string                 `json:"api,omitempty"`
@@ -21,7 +22,7 @@ type LogEntry struct {
 	Raw       string                 `json:"-"`
 }
 
-// parseLogLine parses a single log line into a LogEntry
+// Parses a single log line into a LogEntry
 func parseLogLine(line string) *LogEntry {
 	timestampPattern := regexp.MustCompile(`-->(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3})`)
 	pmIdPattern := regexp.MustCompile(`(^\d+)\|[^\|]+?\s\|`)
@@ -71,54 +72,139 @@ func parseLogLine(line string) *LogEntry {
 	return entry
 }
 
-// formatLogEntry formats a LogEntry into a readable string
+// Formats a LogEntry into a readable string
 func formatLogEntry(entry *LogEntry) string {
 	if entry.Details != nil {
 		prettyDetails, _ := json.MarshalIndent(entry.Details, "", "  ")
-		return fmt.Sprintf("Timestamp: %s\nPM ID: %s\nServer: %s\nModule: %s\nAPI: %s\nEvent: %s\nDetails:\n%s\n",
-			entry.Timestamp, entry.PmId, entry.Server, entry.Module, entry.Api, entry.Event, string(prettyDetails))
+		return fmt.Sprintf("Timestamp: %s\nPM ID: %s\nServer: %s\nModule: %s\nAPI: %s\nEvent: %s\nDetails:\n%s\n%s\n",
+			entry.Timestamp,
+			entry.PmId,
+			entry.Server,
+			entry.Module,
+			entry.Api,
+			entry.Event,
+			string(prettyDetails),
+			strings.Repeat("-", 80))
 	}
 	return entry.Raw
 }
 
-func main() {
-
-	// Getting file path from args
-	filePath := os.Args[1]
-
-	info, err := os.Stat(filePath)
-
-	// Ensuring file exists
-	if err != nil {
-		if os.IsNotExist(err) {
-			fmt.Println("File does not exist")
+// Writes a LogEntry to a file
+func writeLogEntriesToFile(logEntries []string, file *os.File) error {
+	for _, logEntry := range logEntries {
+		_, err := file.WriteString(logEntry)
+		if err != nil {
+			err = fmt.Errorf("error writing to file %s", file.Name())
+			return err
 		}
-		fmt.Println("Could not get file stats", err)
-		return
 	}
+	return nil
+}
 
-	// Ensuring it is not a directory
-	if info.IsDir() {
-		fmt.Println("Given path leads to a directory")
-		return
-	}
-
-	file, err := os.Open(filePath)
+// Checks if a file is directory
+func isFile(filePath string) error {
+	info, err := os.Stat(filePath)
 	if err != nil {
-		fmt.Println("Error opening file:", err)
+		if err == os.ErrNotExist {
+			err = fmt.Errorf("file does not exist: %s", filePath)
+			return err
+		}
+		err = fmt.Errorf("cannot check file stats: %s", err.Error())
+		return err
+	}
+
+	if info.IsDir() {
+		err = fmt.Errorf("file %s is a directory", filePath)
+		return err
+	}
+
+	return nil
+}
+
+// Usage text
+func usage() string {
+	usageElements := []string{
+		"Usage: golog -source=<source-file-path> [-target=<target-file-path>] [-show]",
+		"Options:",
+		"\tsource (required): File path of source file with unformatted logs.",
+		"\ttarget: File path where you want to store the formatted logs. File is created if it does not exist.",
+		"\tshow: Set flag to print formatted logs on console even if -target flag is set",
+	}
+	return strings.Join(usageElements, "\n")
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("Too few arguments!\n%s", usage())
 		return
 	}
-	defer file.Close()
 
-	scanner := bufio.NewScanner(file)
+	// flag for setting an input file path to read unformatted logs from
+	sourceFileUsageText := "File path of source file with unformatted logs"
+	sourceFilePath := flag.String("source", "", sourceFileUsageText)
+
+	// flag for setting an output file path to store the formatted logs into
+	outFileUsageText := "File path where you want to store the formatted logs. File is created if it does not exist"
+	outFilePath := flag.String("target", "", outFileUsageText)
+
+	// flag to show logs even if target file is given
+	showLogsUsageText := "Set flag to print formatted logs on console even if -target flag is set"
+	showLogs := flag.Bool("show", false, showLogsUsageText)
+
+	flag.Parse()
+	if *sourceFilePath == "" {
+		fmt.Printf("No source file specified")
+		return
+	}
+
+	err := isFile(*sourceFilePath)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	// Open source file
+	sourceFile, err := os.Open(*sourceFilePath)
+	if err != nil {
+		fmt.Printf("error opening file %s", *sourceFilePath)
+		return
+	}
+	defer sourceFile.Close()
+
+	scanner := bufio.NewScanner(sourceFile)
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	// Scanning source log lines
+	entries := []string{}
 	for scanner.Scan() {
 		line := scanner.Text()
 		entry := parseLogLine(line)
-		fmt.Println(formatLogEntry(entry))
-		fmt.Println(strings.Repeat("-", 80))
+		entries = append(entries, formatLogEntry(entry))
 	}
 
-	if err := scanner.Err(); err != nil {
-		fmt.Println("Error reading file:", err)
+	if *outFilePath != "" {
+		// Writing formatted logs to file
+		outFile, err := os.Create(*outFilePath)
+		if err != nil {
+			fmt.Printf("error opening/creating output file %s", *outFilePath)
+			return
+		}
+		defer outFile.Close()
+		err = writeLogEntriesToFile(entries, outFile)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		fmt.Printf("formatted logs written to file %s", *outFilePath)
+	}
+
+	if *outFilePath == "" || *showLogs {
+		// Writing logs to console
+		for _, entry := range entries {
+			fmt.Println(entry)
+		}
 	}
 }
