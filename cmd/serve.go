5d9ec92f9358545b15af717edc3c50088a7f5f36
Kashish Sahu <125540624+gitKashish@users.noreply.github.com>
2025-03-10 16:47:23 +0530

    priority to PORT in environment

--- cmd/serve.go
+++ cmd/serve.go
@@ -4,20 +4,27 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"os"
+	"strconv"
+
 	server "github.com/gitKashish/golog/internal"
 	"github.com/spf13/cobra"
 )
 
 // Port number to serve the web app on
-var port int = 2600
+var port int
 
 // serveCmd represents the serve command
 var serveCmd = &cobra.Command{
 	Use:   "serve",
-	Short: "Start serving the golog web app on specified port (default: 2600)",
+	Short: "Start serving the golog web app on specified port (default: 2600, overriden by PORT environment variable)",
 	Long: `Start an HTTP server on the specified port to serve the golog webapp.
 	Default port is 2600 but you can specify a different port using the -p or --port flag.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
+		port, err := strconv.Atoi(os.Getenv("PORT"))
+		if err != nil {
+			port = 2600
+		}
 		server := server.NewServer(port)
 		return server.Serve()
 	},
